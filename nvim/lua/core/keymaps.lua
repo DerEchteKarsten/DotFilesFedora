3eae7cabeea946f3300353d20f2c2599dfa3c385
Karsten <jck@mitwo.de>
2023-06-14 17:25:02 +0200

    initial commit

---
+++ nvim/lua/core/keymaps.lua
@@ -0,0 +1,15 @@
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+vim.opt.backspace = '2'
+vim.opt.showcmd = true
+vim.opt.laststatus = 2
+vim.opt.autowrite = true
+vim.opt.cursorline = false
+vim.opt.autoread = true
+
+vim.opt.shiftwidth = 4
+vim.opt.shiftround = true
+vim.opt.expandtab = true
+
+vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
